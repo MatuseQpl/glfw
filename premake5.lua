62b1842585fce0f0562873daab1c0afc53d04786
MatuseQpl <89225961+MatuseQpl@users.noreply.github.com>
2025-11-27 22:11:38 +0100

    updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -31,8 +32,6 @@ project "GLFW"
 	}
 	filter "system:windows"
 		systemversion "latest"
-		buildoptions { "-std=c11", "-lgdi32" }
-		staticruntime "On"
 
 		files
 		{
@@ -54,5 +53,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
